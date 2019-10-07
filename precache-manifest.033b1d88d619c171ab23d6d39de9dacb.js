436014fbf8cf4c81036fb41be7e9396190366f85
Jonas Silva <jonas.silva@farfetch.com>
2019-10-07 23:28:36 +0100

    Updates

---
+++ precache-manifest.033b1d88d619c171ab23d6d39de9dacb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7447a93720f46cf51e7bfe4b90100bab",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f6bf3aa17b1e513dcc41",
+    "url": "/monster-rolodex/static/css/main.3d4d97fa.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monster-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "f6bf3aa17b1e513dcc41",
+    "url": "/monster-rolodex/static/js/main.b4b7cbba.chunk.js"
+  },
+  {
+    "revision": "22232bd4ca03c1558311",
+    "url": "/monster-rolodex/static/js/runtime-main.b7028ca9.js"
+  }
+]);
